4816a97a2ea8effdb58149e559a55bda31ac1dbc
Andrii Tokar <andrii.tokar@gmail.com>
2023-02-14 22:47:26 +0200

    [2023-02-14] add inArray functions

--- functions/arrayFunctions/array.go
+++ functions/arrayFunctions/array.go
@@ -20,3 +20,47 @@ func StringArrayChunk(strArray []string, size int) [][]string {
 
 	return result
 }
+
+func IntInArray(a []int, x int) bool {
+	return Int64InArray(IntArrayToInt64Array(a), int64(x))
+}
+
+func Int64InArray(a []int64, x int64) bool {
+	if 0 < len(a) {
+		for _, i := range a {
+			if x == i {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func Int32InArray(a []int32, x int32) bool {
+	return Int64InArray(Int32ArrayToInt64Array(a), int64(x))
+}
+
+func IntArrayToInt64Array(a []int) []int64 {
+	res := []int64{}
+
+	if 0 < len(a) {
+		for _, i := range a {
+			res = append(res, int64(i))
+		}
+	}
+
+	return res
+}
+
+func Int32ArrayToInt64Array(a []int32) []int64 {
+	res := []int64{}
+
+	if 0 < len(a) {
+		for _, i := range a {
+			res = append(res, int64(i))
+		}
+	}
+
+	return res
+}
